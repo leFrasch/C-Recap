41bf9f289590ba26b147be9e8b83e18a784c6f02
leFrasch <mburu.johnn3@gmail.com>
2022-04-26 19:49:53 -0700

    Skip some letters

---
+++ skip-alph.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+        char alph = 'a';
+
+
+        while ( alph <= 'z' )
+        {
+		if ( alph != 'e' || alph != 'q')
+		{
+			putchar(alph);
+			alph++;
+		}
+
+	}
+        
+        putchar('\n');
+        return(0);
+
+
+}
+
