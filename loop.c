19e126df4d81f594080318bd0a858f2633981bf5
leFrasch <mburu.johnn3@gmail.com>
2022-04-26 18:51:23 -0700

    The alphabet

---
+++ loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	char alph = 'a';
+        
+
+        while ( alph <= 'z' )
+        {
+                putchar(alph);
+                alph++;
+        }
+        putchar('\n');
+
+        return(0);
+	
+}
