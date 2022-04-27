d73ade4994f03aea478f7ae6d92036b9d1401600
leFrasch <mburu.johnn3@gmail.com>
2022-04-27 12:41:27 -0700

    Hot fix

---
+++ combination.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = '0';
+
+
+	while ( i<= '9')
+	{
+		putchar(i);
+		
+		if ( i != '9' )
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return(0);
+}
