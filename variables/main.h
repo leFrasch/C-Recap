e393df9359759ef2f724fb8c2c751755938c339b
leFrasch <mburu.johnn3@gmail.com>
2022-05-05 04:40:06 -0700

    print a string using a function

---
+++ variables/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+/* these is a function that prints a character */
+
+int _putchar (char c);
+
+
+/*this function prints the alphabet*/
+
+void print_alphabet(void);
+
+#endif
+
